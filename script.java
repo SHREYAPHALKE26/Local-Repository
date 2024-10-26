d19122e226f82dd02fce4c96a532a84afc5c38f6
Shreya Phalke <shreya.phalke24@vit.edu>
2024-10-26 10:34:19 +0530

    Delete script.java

--- script.java
+++
@@ -1,8 +0,0 @@
-function showInfo() {
-    var infoSection = document.getElementById('committee-info');
-    if (infoSection.style.display === 'none') {
-        infoSection.style.display = 'block';
-    } else {
-        infoSection.style.display = 'none';
-    }
-}
